ed0bcb81157b4d5bf355b8cf045bcd4b3f7eb5b4
kxx <132kxx@gmail.com>
2022-07-05 21:43:01 +0900

    Initial Commit

---
+++ AppStuff_BootCamp/AppStuff_BootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStuff_BootCampApp.swift
+//  AppStuff_BootCamp
+//
+//  Created by kxx: on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct AppStuff_BootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
